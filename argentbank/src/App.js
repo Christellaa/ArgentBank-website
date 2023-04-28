6fd88cbf6232ba8e92db37fe3f8ab390ee88b919
Christella De Sousa <desousa.christella@proton.me>
2023-04-28 18:56:32 +0200

    Modify paths and their name

--- argentbank/src/App.js
+++ argentbank/src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import {Routes, Route} from'react-router-dom';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import User from './pages/User';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import "./App.css";
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element= {<Home />}/>
-      <Route path='/sign-in' element= {<SignIn />}/>
-      <Route path='/user' element= {<User />}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
