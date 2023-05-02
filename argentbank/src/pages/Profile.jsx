2099e3ae02446473f3d97fa9da9b1d42e1366b2c
Christella De Sousa <desousa.christella@proton.me>
2023-05-02 16:38:21 +0200

    Fix class to className

--- argentbank/src/pages/Profile.jsx
+++ argentbank/src/pages/Profile.jsx
@@ -7,16 +7,16 @@ function Profile() {
   return (
     <>
       <Navbar />
-      <main class="main bg-dark">
-        <div class="header">
+      <main className="main bg-dark">
+        <div className="header">
           <h1>
             Welcome back
             <br />
             Tony Jarvis!
           </h1>
-          <button class="edit-button">Edit Name</button>
+          <button className="edit-button">Edit Name</button>
         </div>
-        <h2 class="sr-only">Accounts</h2>
+        <h2 className="sr-only">Accounts</h2>
         {accountsData.map(({ id, title, amount, text }) => (
           <Accounts key={id} title={title} amount={amount} text={text} />
         ))}
