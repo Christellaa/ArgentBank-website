a5d3bbccc236a4d9f40f3e47ed45301669b69b50
Christella De Sousa <desousa.christella@proton.me>
2023-05-02 16:36:27 +0200

    Delete two of the three sections and add props

--- argentbank/src/components/Accounts.jsx
+++ argentbank/src/components/Accounts.jsx
@@ -1,31 +1,11 @@
-function Accounts() {
+function Accounts({ title, amount, text }) {
   return (
     <>
       <section class="account">
         <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Checking (x8349)</h3>
-          <p class="account-amount">$2,082.79</p>
-          <p class="account-amount-description">Available Balance</p>
-        </div>
-        <div class="account-content-wrapper cta">
-          <button class="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section class="account">
-        <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Savings (x6712)</h3>
-          <p class="account-amount">$10,928.42</p>
-          <p class="account-amount-description">Available Balance</p>
-        </div>
-        <div class="account-content-wrapper cta">
-          <button class="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section class="account">
-        <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p class="account-amount">$184.30</p>
-          <p class="account-amount-description">Current Balance</p>
+          <h3 class="account-title">{title}</h3>
+          <p class="account-amount">{amount}</p>
+          <p class="account-amount-description">{text}</p>
         </div>
         <div class="account-content-wrapper cta">
           <button class="transaction-button">View transactions</button>
