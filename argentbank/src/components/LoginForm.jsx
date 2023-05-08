f05dda5a9c81f3f4977b939c4f4c7ee590ebe2bf
Christella De Sousa <desousa.christella@proton.me>
2023-05-08 20:51:45 +0200

    Add store and login logic to form

--- argentbank/src/components/LoginForm.jsx
+++ argentbank/src/components/LoginForm.jsx
@@ -1,20 +1,62 @@
+import { useStore } from "react-redux";
+import { useEffect } from "react";
+import { useState } from "react";
+import { PostUser } from "../features/posts/postUser";
+
 function LoginForm() {
+  const store = useStore();
+  useEffect(() => {
+    PostUser(store);
+  }, [store]);
+
+  const user = {
+    username: document.getElementById("username"),
+    password: document.getElementById("password"),
+  };
+
+  const [submit, setSubmit] = useState(false);
+
+  setTimeout(() => {
+    setSubmit(false);
+  }, 3000);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setSubmit(true);
+    alert("you have submitted the form!");
+  }
+
   return (
-    <form>
-      <div className="input-wrapper">
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
-      </div>
-      <div className="input-wrapper">
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
-      </div>
-      <div className="input-remember">
-        <input type="checkbox" id="remember-me" />
-        <label htmlFor="remember-me">Remember me</label>
-      </div>
-      <button className="sign-in-button">Sign In</button>
-    </form>
+    <>
+      {submit}
+      <form onSubmit={handleSubmit}>
+        <div className="input-wrapper" disabled={submit}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            required
+            value={FormData.username || ""}
+          />
+        </div>
+        <div className="input-wrapper" disabled={submit}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            value={FormData.password || ""}
+          />
+        </div>
+        <div className="input-remember">
+          <input type="checkbox" id="remember-me" />
+          <label htmlFor="remember-me">Remember me</label>
+        </div>
+        <button disabled={submit} type="submit" className="sign-in-button">
+          Sign In
+        </button>
+      </form>
+    </>
   );
 }
 
