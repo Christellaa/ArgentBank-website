c181f0bc6a110cc4aa9f4d6fd2dddda5ea1667cb
Christella De Sousa <desousa.christella@proton.me>
2023-05-08 20:40:58 +0200

    Create json file for home features

--- argentbank/src/pages/Home.jsx
+++ argentbank/src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import Navbar from "../components/Navbar";
 import Banner from "../components/Banner";
+import featuresData from "../data/featuresData.json";
+import Features from "../components/Features";
 import Footer from "../components/Footer";
-
-import iconChat from "../img/icon-chat.png";
-import iconMoney from "../img/icon-money.png";
-import iconSecurity from "../img/icon-security.png";
 
 function Home() {
   return (
@@ -16,31 +14,9 @@ function Home() {
         <Banner />
         <section className="features">
           <h2 className="sr-only">Features</h2>
-          <div className="feature-item">
-            <img src={iconChat} alt="Chat Icon" className="feature-icon" />
-            <h3 className="feature-item-title">You are our #1 priority</h3>
-            <p>
-              Need to talk to a representative? You can get in touch through our
-              24/7 chat or through a phone call in less than 5 minutes.
-            </p>
-          </div>
-          <div className="feature-item">
-            <img src={iconMoney} alt="Chat Icon" className="feature-icon" />
-            <h3 className="feature-item-title">
-              More savings means higher rates
-            </h3>
-            <p>
-              The more you save with us, the higher your interest rate will be!
-            </p>
-          </div>
-          <div className="feature-item">
-            <img src={iconSecurity} alt="Chat Icon" className="feature-icon" />
-            <h3 className="feature-item-title">Security you can trust</h3>
-            <p>
-              We use top of the line encryption to make sure your data and money
-              is always safe.
-            </p>
-          </div>
+          {featuresData.map(({ id, img, title, text }) => (
+            <Features key={id} img={img} title={title} text={text} />
+          ))}
         </section>
       </main>
       <Footer />
