11b72dfd3cc351eee83c434c1eeb91beb087ebdf
Christella De Sousa <desousa.christella@proton.me>
2023-05-15 11:54:26 +0200

    Delete counter reducer from store

--- argentbank/src/app/store.js
+++ argentbank/src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
-import postUserReducer from "../features/posts/postUser";
+import postUserReducer from "../features/posts/postUserSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     postUser: postUserReducer,
   },
 });
