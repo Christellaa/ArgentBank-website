e9e10a16a9e8c47d12ae9a13e3d1f0172749506f
Christella De Sousa <desousa.christella@proton.me>
2023-05-15 11:57:50 +0200

    Create slice and add reducers

--- argentbank/src/features/posts/postUserSlice.js
+++ argentbank/src/features/posts/postUserSlice.js
@@ -6,3 +6,26 @@ const initialState = {
   data: null,
   error: null,
 };
+
+const postUserSlice = createSlice({
+  name: "postUser",
+  initialState: { ...initialState },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(sendForm.pending, (state) => {
+        state.loading = true;
+        state.data = null;
+        state.error = null;
+      })
+      .addCase(sendForm.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload.body.token;
+      })
+      .addCase(sendForm.rejected, (state, action) => {
+        state.loading = false;
+        state.data = null;
+        state.error = action.error.message;
+      });
+  },
+});
