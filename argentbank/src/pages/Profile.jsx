15801d51a7fa092c7243076418fad549af25d7d8
Christella De Sousa <desousa.christella@proton.me>
2023-05-18 18:09:37 +0200

    Force users not logged in to log in to be able to see their profile

--- argentbank/src/pages/Profile.jsx
+++ argentbank/src/pages/Profile.jsx
@@ -2,26 +2,41 @@ import Navbar from "../components/Navbar";
 import accountsData from "../data/accountsData.json";
 import Accounts from "../components/Accounts";
 import Footer from "../components/Footer";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 function Profile() {
+  const userLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   return (
     <>
-      <Navbar />
-      <main className="main bg-dark">
-        <div className="header">
-          <h1>
-            Welcome back
-            <br />
-            Tony Jarvis!
-          </h1>
-          <button className="edit-button">Edit Name</button>
+      {userLoggedIn === true ? (
+        <>
+          <Navbar />
+          <main className="main bg-dark">
+            <div className="header">
+              <h1>
+                Welcome back
+                <br />
+                Tony Jarvis!
+              </h1>
+              <button className="edit-button">Edit Name</button>
+            </div>
+            <h2 className="sr-only">Accounts</h2>
+            {accountsData.map(({ id, title, amount, text }) => (
+              <Accounts key={id} title={title} amount={amount} text={text} />
+            ))}
+          </main>
+          <Footer />
+        </>
+      ) : (
+        <div>
+          <p>Vous n'avez pas accès à cette page. Veuillez vous connecter.</p>
+          <Link to="/login" className="main-nav-item">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Link>
         </div>
-        <h2 className="sr-only">Accounts</h2>
-        {accountsData.map(({ id, title, amount, text }) => (
-          <Accounts key={id} title={title} amount={amount} text={text} />
-        ))}
-      </main>
-      <Footer />
+      )}
     </>
   );
 }
