82f5a2d7b9f80b5fb9571e81fe829d05993750aa
Christella De Sousa <desousa.christella@proton.me>
2023-05-19 16:02:23 +0200

    Dispatch userToken with token of localStorage

--- argentbank/src/App.js
+++ argentbank/src/App.js
@@ -4,8 +4,19 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import "./App.css";
+import { userToken } from "./features/auth/authSlice";
+import { useDispatch } from "react-redux";
+import { store } from "./app/store";
+import { fetchUser } from "./features/profile/fetchUserAPI";
 
 function App() {
+  const dispatch = useDispatch();
+
+  if (localStorage.getItem("token") !== null) {
+    const token = localStorage.getItem("token");
+    store.dispatch(userToken(token));
+    dispatch(fetchUser());
+  }
   return (
     <Routes>
       <Route path="/" element={<Home />} />
