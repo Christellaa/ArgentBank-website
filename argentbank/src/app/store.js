29da0e4ec9bd0d47812a74d6bf0fd68cb8602995
Christella De Sousa <desousa.christella@proton.me>
2023-05-19 09:27:06 +0200

    Add user slice to the store

--- argentbank/src/app/store.js
+++ argentbank/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import userSlice from "../features/profile/userSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    user: userSlice,
   },
 });
